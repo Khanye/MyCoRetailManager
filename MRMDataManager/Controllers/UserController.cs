35c0931d0fb5a022086a4f6b992933f9de196635
khany <khanyeinc@gmail.com>
2023-12-29 00:29:39 +0200

    Add and Remove Roles on UserManagement
    Adds the functionality and upadates the view to add and remove roles on users by the the Admin

--- MRMDataManager/Controllers/UserController.cs
+++ MRMDataManager/Controllers/UserController.cs
@@ -34,7 +34,7 @@ namespace MRMDataManager.Controllers
             using (var context = new ApplicationDbContext())
             {
                 var userStore = new UserStore<ApplicationUser>(context);
-                var userManager = new UserManager<ApplicationUser>(userStore);
+                var userManager = new UserManager<ApplicationUser>(userStore); 
 
                 var users = userManager.Users.ToList();
                 var roles = context.Roles .ToList();
@@ -59,5 +59,44 @@ namespace MRMDataManager.Controllers
             return output;
         }
 
+        [Authorize(Roles = "Admin")]
+        [HttpGet]
+        [Route("api/User/Admin/GetAllRoles")]
+        public Dictionary<string,string> GetAllRoles()
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                var roles = context.Roles.ToDictionary(x => x.Id , x => x.Name);
+                return roles;
+            }
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpPost]
+        [Route("api/User/Admin/AddRole")]
+        public void AddARole(UserRolePairModel pairing)
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                var userStore = new UserStore<ApplicationUser>(context);
+                var userManager = new UserManager<ApplicationUser>(userStore);
+
+                userManager.AddToRole(pairing.UserId, pairing.RoleName);
+            }
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpPost]
+        [Route("api/User/Admin/RemoveRole")]
+        public void RemoveARole(UserRolePairModel pairing)
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                var userStore = new UserStore<ApplicationUser>(context);
+                var userManager = new UserManager<ApplicationUser>(userStore);
+
+                userManager.RemoveFromRole(pairing.UserId, pairing.RoleName);
+            }
+        }
     }
 }
