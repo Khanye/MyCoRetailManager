8eca8d938a389d993d88f5e08210eb1e9d5737c1
khany <khanyeinc@gmail.com>
2023-12-29 13:05:10 +0200

    Added New API and Moved Over Functionality

---
+++ MRMApi/Controllers/InventoryController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MRMDataManager.Library.DataAccess;
+using MRMDataManager.Library.Models;
+
+namespace MRMApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class InventoryController : ControllerBase
+    {
+        [Authorize(Roles = "Admin,Manager")]
+        public List<InventoryModel> Get()
+        {
+            InventoryData data = new InventoryData();
+            return data.GetInventory();
+        }
+
+        [Authorize(Roles = "Admin")]
+        public void Post(InventoryModel model)
+        {
+            InventoryData data = new InventoryData();
+            data.SaveInventoryRecord(model);
+        }
+    }
+}
---
+++ MRMApi/Controllers/SaleController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MRMDataManager.Library.DataAccess;
+using MRMDataManager.Library.Models;
+using System.Security.Claims;
+
+namespace MRMApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class SaleController : ControllerBase
+    {
+        [Authorize(Roles = "Cashier")]
+        public void Post(SaleModel sale)
+        {
+            SaleData data = new SaleData();
+            string userid = User.FindFirstValue(ClaimTypes.NameIdentifier); //RequestContext.Principal.Identity.GetUserId();
+
+            data.SaveSale(sale, userid);
+        }
+
+        [Authorize(Roles = "Admin,Manager")]
+        [Route("GetSalesReport")]
+        public List<SaleReportModel> GetSaleReports()
+        {
+            SaleData data = new SaleData();
+
+            return data.GetSaleReport();
+
+        }
+    }
+}
